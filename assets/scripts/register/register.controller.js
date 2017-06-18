11a8747a9ec692598caaf583c10b92e83eaa1798
guuibayer <guiilherme.bayer@gmail.com>
2017-06-18 18:21:10 -0300

    Register spot section

--- assets/scripts/register/register.controller.js
+++ assets/scripts/register/register.controller.js
@@ -2,6 +2,30 @@ angular
   .module('RegisterController', [])
   .controller('RegisterController', RegisterController)
 
-  function RegisterController() {
+  RegisterController.$inject = ['NgMap']
+
+  function RegisterController(NgMap) {
     var vm = this
+
+    vm.position = []
+    vm.register = register
+    vm.nextStep = nextStep
+
+    function nextStep() {
+      let modal = document.querySelector('.register-modal')
+      modal.classList.remove('hidden')
+    }
+
+    function register() {
+
+
+      NgMap
+        .getMap()
+        .then(map => {
+          vm.position = {
+            lat: map.getCenter().lat(),
+            lng: map.getCenter().lng()
+          }
+        })
+    }
   }
