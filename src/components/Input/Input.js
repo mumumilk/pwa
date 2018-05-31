53aebe44c9eeab61e673fedd6cda1b857144a7fe
guuibayer <guiilherme.bayer@gmail.com>
2018-05-30 22:15:55 -0300

    Criando componente de input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import classnames from 'classnames'
 
+const MODIFIER = 'input-group'
+
 const Input = ({
   black,
   white,
@@ -9,24 +11,33 @@ const Input = ({
   medium,
   text,
   onClick,
+  onChange,
   value,
+  label,
+  id,
   ...props
 }) => {
-  const classes = classnames('input', {
-    black,
-    white,
-    full,
-    large,
-    medium
+  const classes = classnames({
+    [`${MODIFIER}--black`]: black,
+    [`${MODIFIER}--white`]: white,
+    [`${MODIFIER}--large`]: large,
+    [`${MODIFIER}--medium`]: medium
   })
 
   return (
-    <input
-      type="text"
-      className={classes}
-      onClick={onClick}
-      value={value}
-      {...props} />
+    <div className={`input-group ${classes}`}>
+      <label className="input-group__label" htmlFor={id}>{label}</label>
+
+      <input
+        id={id}
+        name={id}
+        className="input-group__input"
+        type="text"
+        onClick={onClick}
+        onChange={onChange}
+        value={value}
+        {...props} />
+    </div>
   )
 }
 
--- src/pages/spots/NewSpot.js
+++ src/pages/spots/NewSpot.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import Input from "../../components/Input/Input"
+
 class NewSpot extends Component {
   constructor(props) {
     super(props)
@@ -8,8 +10,13 @@ class NewSpot extends Component {
   render() {
     return (
       <div className="new-spot">
-        <form>
+        <h1 className="new-spot__title">Cadastrar novo pico</h1>
 
+        <form className="new-spot__form">
+          <Input
+            medium
+            id="name"
+            label="Nome do pico" />
         </form>
       </div>
     )
