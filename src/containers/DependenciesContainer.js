ab8bc5a3c71afeed5b5aac6b0505d58f1046b338
guuibayer <guiilherme.bayer@gmail.com>
2018-06-19 19:25:36 -0300

    Alterando para withRouter ao invês de HOC do react-router-dom

--- src/containers/DependenciesContainer.js
+++ src/containers/DependenciesContainer.js
@@ -1,21 +1,20 @@
 import React from 'react'
 
-import { withFirebase } from 'react-redux-firebase'
-import { BrowserRouter } from 'react-router-dom'
 import { compose } from 'recompose'
+
+import { withFirebase } from 'react-redux-firebase'
+import { withRouter } from 'react-router-dom'
 
 const DependenciesContainer = Component => {
   class Dependencies extends React.Component {
     render() {
       return (
-        <BrowserRouter>
-          <Component {...this.props} />
-        </BrowserRouter>
+        <Component {...this.props} />
       )
     }
   }
 
-  return compose(withFirebase)(Dependencies)
+  return compose(withFirebase, withRouter)(Dependencies)
 }
 
 export default DependenciesContainer
